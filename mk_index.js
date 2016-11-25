0e438db55e6cd800f780a34b673f5ead2c180ed1
dorisofsky <dorisofsky@gmail.com>
2016-11-25 17:30:21 +0800

    為什麼timebar的bar都不見了，改回nfa2看看

--- mk_index.js
+++ mk_index.js
@@ -40,7 +40,7 @@
     function draw() {
 
       var csv = d3.dsv(",", "text/csv;charset=big5");
-      csv("nfa3.csv", function(data) {
+      csv("nfa2.csv", function(data) {
 
         var timeAllparse = d3.time.format("%Y/%m/%e %H:%M").parse,
             dateformat = d3.time.format("%Y/%m/%d"), 
